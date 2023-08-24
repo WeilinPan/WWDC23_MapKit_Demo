a36c083dea3398eb23c66e3ecbe7fed9fd53edcd
WeilinPan <s71440@gmail.com>
2023-08-24 14:35:21 +0800

    feat: poject init

---
+++ WWDC23_MapKit_Demo/WWDC23_MapKit_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC23_MapKit_Demo
+//
+//  Created by 潘威霖 on 2023/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC23_MapKit_Demo/WWDC23_MapKit_Demo/WWDC23_MapKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC23_MapKit_DemoApp.swift
+//  WWDC23_MapKit_Demo
+//
+//  Created by 潘威霖 on 2023/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WWDC23_MapKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
